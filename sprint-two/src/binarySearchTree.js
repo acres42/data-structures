20486482e42da309b4fc12d4cb1c44cf29756660
AC Roselee <acroselee@gmail.com>
2019-01-13 18:34:14 -0800

    binary search test changed to work with psuedoclassical instantiation. all tests passing as expected on BST and HashTable

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,57 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+
+
+BinarySearchTree.prototype.insert = function(value) {
+  //left
+  if(value < this.value){
+    if(!this.left){
+      this.left = new BinarySearchTree(value);
+    } else{
+      this.left.insert(value);
+    }
+    //right
+  } else if (value > this.value){
+    if(!this.right){
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+BinarySearchTree.prototype.contains = function(value) {
+  if(value === this.value){
+    return true;
+  }
+
+  if(this.left && value < this.value){
+    return this.left.contains(value);
+  }
+
+  if(this.right && value > this.value){
+    return this.right.contains(value);
+  }
+
+  return false;
+
+};
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  //stop case
+  callback(this.value);
+
+  if(this.left){
+    this.left.depthFirstLog(callback);
+  }
+
+  if(this.right){
+    this.right.depthFirstLog(callback);
+  }
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,20 +5,55 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  if (!bucket) {
+    bucket = [];
+    this._storage.set(index, bucket);
+  }
+
+  bucket.forEach(function(x) {
+    if (x[0] === k) {
+      x[1] === v;
+    }
+  });
+
+  bucket.push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var found = undefined;
+
+  if (!this._storage.get(index)) {
+    return null;
+  }
+
+  bucket.forEach(function(x) {
+    if (x[0] === k) {
+      found = x[1];
+    }
+  });
+  return found;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  var bucket = this._storage.get(index);
 
+  if (!this._storage.get(index)) {
+    return null;
+  }
+
+  bucket.forEach(function(x, index) {
+    if (x[0] === k){
+      bucket.splice(index, 1);
+    }
+  });
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
